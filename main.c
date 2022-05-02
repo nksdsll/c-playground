4243c766b29ef223f301b1a209746c3f380abc38
nksdsll <99758174+nksdsll@users.noreply.github.com>
2022-05-02 21:34:37 +0200

    print ASCII table

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, 2!\n");
-    return 0;
+
+int print_whole_ascii_table() {
+    char i=0;
+    for(i=0; i<127; ++i){
+        printf("%d\n",i);
+    }
 }
+
+
+int print_whole_ascii_table_using_while() {
+    char i=0;
+    while( i<127){
+        printf("%d-%c\n", i, i);
+        i++;
+    }
+}
+
+
+int main() {
+    print_whole_ascii_table_using_while();
+    return 1;
+}
+
+
+
